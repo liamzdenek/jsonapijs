832a38be64184d7c03b61e22f655259f5114cfe9
Liam Zdenek <liamzdenek@gmail.com>
2016-09-28 16:11:31 +0000

    Packaging the library

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 var express = require('express')
     ,bodyparser = require('body-parser')
     ,port = process.env.PORT || 3000
-    ,RamResource = require('./resources/RamResource.js')
-    ,LocalIdByField = require('./relationships/LocalIdByField.js')
-    ,ForeignIdByField = require('./relationships/ForeignIdByField.js')
+    ,ja = require('./lib')
     ;
 
 var app = express();
 
 app.use( bodyparser.json() );
 
-var config = require('./config.js')();
+var config = ja.Config();
 
 {
-    let cats = RamResource();
+    let cats = ja.RamResource();
     cats.push("cat1", {"asdf": "123", "owner_id": "person1"});
     cats.push("cat2", {"asdf": "223", "owner_id": "person2"});
     cats.push("cat3", {"asdf": "323", "owner_id": "person3"});
@@ -21,7 +19,7 @@ var config = require('./config.js')();
     config.push_resource("cats", cats);
 }
 {
-    let people = RamResource();
+    let people = ja.RamResource();
     people.push("person1", {"asdf": "123"});
     people.push("person2", {"asdf": "223"});
     people.push("person3", {"asdf": "323"});
@@ -29,7 +27,7 @@ var config = require('./config.js')();
     config.push_resource("people", people);
 }
 {
-    let posts = RamResource();
+    let posts = ja.RamResource();
     posts.push("1", {"body": "adfadfasdfasdfasdf", "author_id": "person1"});
     posts.push("2", {"body": "adfadfasdfasdfasdf", "author_id": "person1"});
     posts.push("3", {"body": "adfadfasdfasdfasdf", "author_id": "person1"});
@@ -37,13 +35,13 @@ var config = require('./config.js')();
     config.push_resource("posts", posts);
 }
 {
-    config.push_relationship("cats", "owner", LocalIdByField("owner_id", "people"));
+    config.push_relationship("cats", "owner", ja.LocalIdByField("owner_id", "people"));
 }
 {
-    config.push_relationship("people", "posts", ForeignIdByField("author_id", "posts"));
+    config.push_relationship("people", "posts", ja.ForeignIdByField("author_id", "posts"));
 }
 
-require('./routes.js')(config, app);
+ja.Routes(config, app);
 
 app.listen(port, function() {
     console.log("Listening");
