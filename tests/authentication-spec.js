b5360d3bbca8636755ca08a4d94363fafab44f15
Liam Zdenek <liamzdenek@gmail.com>
2016-10-10 19:00:04 +0000

    Automatically hiding password field from AuthenticationResource

--- tests/authentication-spec.js
+++ tests/authentication-spec.js
@@ -38,8 +38,17 @@ function check_login(id) {
         .get(BASE_URL+"/session/mine?include=logged_in_as")
         .addHeader("Authorization", "Bearer "+id)
         .afterJSON(function(json) {
-            console.log("INCLUDED: ", json);
+            console.log("INCLUDED: ", JSON.stringify(json));
             expect(json.included.length).toBe(1,"Expected included to contain the user record");
         })
     .toss();
+
+    common.wrap_success(frisby.create('Ensure /users/2 does not send back "password" field'))
+        .get(BASE_URL+"/users/2")
+        .afterJSON(function(json) {
+            expect(typeof json.data).toBe("object", "Expected exactly one response object");
+            expect("password" in json.data.attributes).toBe(false, "The attributes of the primary response data should not contain the 'password' key");
+            expect("username" in json.data.attributes).toBe(true, "The attributes of the primary response should contain the 'password' key");
+        })
+    .toss()
 }
